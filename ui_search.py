22f60048c986c686f665dd4c508eeeaa7f5d0958
cep2 <clifporter@gmail.com>
2016-09-21 15:12:41 -0700

    A1

--- ui_search.py
+++ ui_search.py
@@ -6,7 +6,7 @@ This is a temporary script file.
 """
 
 from collections import deque
-#import Queue
+import queue as Q
 import sys
 
 #-----------------------------------------------------------
@@ -24,7 +24,17 @@ class Vertex(object):
 	def connect(self, other_vertex):
 		self.edge_list.append(other_vertex)
 		print('Added edge between Vertex ' + self.get_label() + ' and Vertex '+ other_vertex.get_label())
-
+		
+	def weighted_connect(self, other_vertex,weight):
+		self.edge_list.append( (other_vertex, weight) )
+		print('Added edge between Vertex ' + self.get_label() + ' and Vertex '+ other_vertex.get_label() + ' with weight of ' + str(weight) )
+	
+	def get_edge_weight(self, other_vertex):
+		for edge in self.edge_list:
+			if other_vertex == edge[0]:
+				return edge[1]
+		return None
+		
 	def get_edge_iterator(self):
 		return iter(self.edge_list)
 
@@ -62,10 +72,61 @@ class Graph(object):
 			self.adj_list[v_one].connect(self.adj_list[v_two])
 			if not self.directed:
 				self.adj_list[v_two].connect(self.adj_list[v_one])
+				
+	def add_weighted_edge(self,v_one,v_two,weight):
+		if v_one in self.adj_list and v_two in self.adj_list:
+			self.adj_list[v_one].weighted_connect(self.adj_list[v_two], weight)
+			if not self.directed:
+				self.adj_list[v_two].weighted_connect(self.adj_list[v_one], weight)
+				
 	def reset_vertices(self):
 		for vertex_label in self.adj_list:
 			self.adj_list[vertex_label].visited = False
-    #-----------------------------------------------------------
+	#-----------------------------------------------------------
+	def uniform_cost_search(self,start,goal):
+		print('Running Uniform Cost Search between vertex ' + start + ' and Vertex ' + goal )
+		level = dict()
+		parent = dict()
+		total_weight = dict()
+		edge_weight = 0
+		frontier = Q.PriorityQueue() 
+		frontier.put( (0, start) )			#Use priority queue put. 
+		parent[start] = None
+		level[start] = 0
+		total_weight[start] = 0
+		
+		solved = False
+		while not frontier.empty() and not solved:
+			vertex = frontier.get()														#Get frontier
+			self.adj_list[vertex].set_visited()										#Set Visited
+			neighbors = self.adj__list[vertex].get_edge_iterator()				#Grab neighbors list.
+			for neighbor in neighbors:
+				if not solved:
+					if not neighbor.is_visited():
+						parent[neighbor.get_label()] = vertex 						#Set parent in dictionary.
+						level[neighbor.get_label()] = level[vertex] + 1			#Set the level in dictionary.
+						edge_weight = vertex.get_edge_weight(neighbor.get_label())	#get the weight of the edge vertex->neighbor
+						total_weight[neighbor.get_label()] = total_weight[vertex] + edge_weight
+						if neighbor.get_label() == goal:								#Check if solved
+							solved = True
+						frontier.put( (total_weight[neighbor.get_label()], neighbor.get_label()  ) )				#Add to frontier queue if not solved.
+						
+		if solved:
+			print('Solution Found:')
+			while(neighbor):
+				print(neighbor.to_string())
+				if parent[neighbor.get_label()]:
+					neighbor = self.adj_list[parent[neighbor.get_label()]];
+				else:
+					break
+		else:
+				print('No Solution Found!')
+		self.reset_vertices()
+		
+		
+		
+	
+	#-----------------------------------------------------------
 	def breadth_first_search(self,start,goal):
 		print('Running Breadth First Search between Vertex ' + start + ' and Vertex ' + goal )
 		level = dict()
@@ -148,6 +209,8 @@ if __name__ == '__main__':
 	graph.add_vertex('E')
 	graph.add_vertex('F')
 	graph.add_vertex('G')
+	
+	'''
 	graph.add_edge('S', 'A')
 	graph.add_edge('S', 'D')
 	graph.add_edge('A', 'B')
@@ -156,6 +219,17 @@ if __name__ == '__main__':
 	graph.add_edge('B', 'E')
 	graph.add_edge('E', 'F')
 	graph.add_edge('F', 'G')
+	'''
+	
+	graph.add_weighted_edge('S', 'A', 1)
+	graph.add_weighted_edge('S', 'D', 3)
+	graph.add_weighted_edge('A', 'B', 2)
+	graph.add_weighted_edge('D', 'E', 3)
+	graph.add_weighted_edge('B', 'C', 4)
+	graph.add_weighted_edge('B', 'E', 5)
+	graph.add_weighted_edge('E', 'F', 2)
+	graph.add_weighted_edge('F', 'G', 4)
  
-	graph.breadth_first_search('S','G')
-	graph.depth_first_search('S','G')
+	#graph.breadth_first_search('S','G')
+	#graph.depth_first_search('S','G')
+	graph.uniform_cost_search('S', 'G')
